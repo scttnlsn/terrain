da36402a4d4f9f815c3abbdca6ea4f640332f19c
Scott Nelson <scott@scottnelson.co>
2016-05-04 13:16:07 -0400

    Added resource updating

--- lib/terrain/resource.rb
+++ lib/terrain/resource.rb
@@ -14,8 +14,11 @@ module Terrain
 
     included do
       def create
-        record = resource.create!(permitted_params)
-        render json: record, status: 201
+        render json: create_record, status: 201
+      end
+
+      def update
+        render json: update_record(load_record)
       end
 
       private
@@ -26,6 +29,19 @@ module Terrain
 
       def permitted_params
         params.permit(*self.class.permit)
+      end
+
+      def load_record
+        resource.find(params[:id])
+      end
+
+      def create_record
+        resource.create!(permitted_params)
+      end
+
+      def update_record(record)
+        record.update_attributes!(permitted_params)
+        record
       end
     end
   end
--- spec/terrain/resource_spec.rb
+++ spec/terrain/resource_spec.rb
@@ -8,7 +8,7 @@ describe 'Terrain::Resource', type: :controller do
   end
 
   describe '#create' do
-    let(:params) { attributes_for(:example) }
+    let(:params) { ActionController::Parameters.new(attributes_for(:example)) }
 
     it 'responds with 201 status' do
       post :create, params
@@ -25,10 +25,42 @@ describe 'Terrain::Resource', type: :controller do
     end
 
     context 'invalid params' do
-      let(:params) { attributes_for(:example, foo: nil) }
+      let(:params) { ActionController::Parameters.new(attributes_for(:example, foo: nil)) }
 
       it 'raises error' do
         expect { post :create, params }.to raise_error ActiveRecord::RecordInvalid
+      end
+    end
+  end
+
+  describe '#update' do
+    let!(:record) { create(:example) }
+    let(:attrs) { attributes_for(:example) }
+    let(:params) { ActionController::Parameters.new(attrs.merge(id: record.id)) }
+
+    it 'responds with 200 status' do
+      patch :update, params
+      expect(response.status).to eq 200
+    end
+
+    it 'updates record' do
+      patch :update, params
+      record.reload
+      expect(record.foo).to eq params[:foo]
+      expect(record.bar).to eq params[:bar]
+      expect(record.baz).to eq params[:baz]
+    end
+
+    it 'responds with serialized record' do
+      patch :update, params
+      expect(response.body).to eq serialize(record.reload).to_json
+    end
+
+    context 'invalid params' do
+      let(:attrs) { attributes_for(:example, foo: nil) }
+
+      it 'raises error' do
+        expect { patch :update, params }.to raise_error ActiveRecord::RecordInvalid
       end
     end
   end
